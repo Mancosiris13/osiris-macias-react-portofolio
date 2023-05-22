4d28fc3c631c09d829d4443edfbc43fc2e972e08
user <osifirsmafias99@gmail.com>
2023-05-21 18:03:04 -0600

    added task app application to portfolio section

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -5,9 +5,11 @@ import port3 from '../media/port3.png';
 import port4 from '../media/port4.png';
 import port5 from '../media/port5.png';
 import port6 from '../media/port6.png';
+import taskApp from '../media/TaskApp.png';
 import translations from '../languages/PortfolioEN-ES.json';
 import ProjectsTranslations from '../languages/ProjectsDescription.json';
 import WebsitePreview from './WebsitePreview';
+import { AiFillStar } from 'react-icons/ai';
 
 const Portofolio = ({ language }) => {
   const english = translations[0].en;
@@ -39,6 +41,26 @@ const Portofolio = ({ language }) => {
           </p>
         </div>
         <div className=" flex flex-col gap-10 py-10 lg:flex-row lg: flex-wrap"></div>
+        <div className="my-20 relative">
+          <div className="md:absolute md:top-0 md:left-0 md:z-10 sm:static sm:top-auto sm:left-auto sm:z-auto">
+            <AiFillStar className="inline-block text-yellow-400 font-bold text-xl m-3" />
+            <span className="inline-block font-semibold">
+              Most Recent Project
+            </span>
+            <AiFillStar className="inline-block text-yellow-400 font-bold text-xl m-3" />
+          </div>
+          <WebsitePreview
+            url={'https://osiris-task-app.herokuapp.com/dashboard'}
+            thumbnail={taskApp}
+            language={language}
+            traductions={
+              language === 'en'
+                ? ProjectsTranslations[4]['task-app'].en
+                : ProjectsTranslations[4]['task-app'].es
+            }
+          />
+        </div>
+
         <div className="my-20">
           <WebsitePreview
             url={
